6edf33644b134222d495eba3374ed21ea7b761bf
estudiante <estudiante@LAB310>
2017-10-13 10:11:39 -0600

    proyecto parte login

---
+++ src/Archivos/LoginArchivo.java
@@ -0,0 +1,42 @@
+
+package Archivos;
+
+import java.io.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class LoginArchivo {
+    
+    public String readarchivo(){
+        String inString = "";
+        try {
+            File archivo = new File("Login.txt");
+            FileReader leer = new FileReader(archivo);
+            BufferedReader inStream = new BufferedReader(leer);
+            inString = inStream.readLine();
+            inStream.close();
+
+        } catch (IOException e) {
+            System.out.println("no se ha podido leer el archivo" + e);
+        }
+        return inString;
+    }
+    public static void registro(String username, String password, String cedula, String correo) {
+        try {
+            ArrayList signup = new ArrayList();
+            signup.add(username);
+            signup.add(password);
+            signup.add(cedula);
+            signup.add(correo);
+            File archivo = new File("Login.txt");
+            FileWriter escribir = new FileWriter(archivo, true);
+            BufferedWriter outStream = new BufferedWriter(escribir);
+            for (int k = 0; k < signup.size(); k++) {
+                outStream.write(signup.get(k).toString() + ",");
+            }
+            outStream.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "No se ha podido registrar el archivo" + e);
+        }
+    }
+}
---
+++ src/Logica/LoginLogica.java
@@ -0,0 +1,51 @@
+package Logica;
+
+import Archivos.LoginArchivo;
+import javax.swing.JOptionPane;
+
+public class LoginLogica {
+
+    public static void verification(String username, String password) {
+        LoginArchivo archivo = new LoginArchivo();
+        String arch = archivo.readarchivo();
+        String[] info = arch.split(",");
+        for (int i = 0; i < info.length; i += 4) {
+            if (username.equals(info[i]) && password.equals(info[i + 1])) {
+                JOptionPane.showMessageDialog(null, "Inicio de sesion correcto");
+                break;
+            }
+            else if(info.length==i+4){
+                JOptionPane.showMessageDialog(null, "Datos ingresados erroneos");
+            }
+        }
+    }
+
+    public static void registro(String username, String password, String cedula, String correo) {
+        LoginArchivo archivo = new LoginArchivo();
+        String arch = archivo.readarchivo();
+        String[] info = arch.split(",");
+        for (int i = 0; i < info.length; i += 4) {
+            if (username.equals(info[i]) && cedula.equals(info[i + 2]) && correo.equals(info[i + 3])) {
+                JOptionPane.showMessageDialog(null, "Cuenta existente, verifique la informacion o inicie sesion");
+                break;
+            }
+            else if (correo.equals(info[i + 3])) {
+                JOptionPane.showMessageDialog(null, "Correo existente");
+                break;
+            }
+            else if (cedula.equals(info[i + 2])) {
+                JOptionPane.showMessageDialog(null, "Cedula existente");
+                break;
+            }
+            else if (username.equals(info[i])) {
+                JOptionPane.showMessageDialog(null, "Username existente");
+                break;
+            }
+            else if(info.length==i+4){
+                LoginArchivo.registro(username, password, cedula, correo);
+                JOptionPane.showMessageDialog(null, "Cuenta creada");
+                
+            }
+        }
+    }
+}
